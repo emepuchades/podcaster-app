a9b844c31683830619e220ab43a39d4fb3106785
María Puchades <100128850+emepuchades@users.noreply.github.com>
2024-04-08 10:52:44 +0200

    feat: add loader in header

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 function Card({title, artist, img, id}) {
   return (
-    <Link to={`/podcast/${id}`}>
+    <Link to={`/podcast/${id}`} className="link-card">
       <div className="container">
         <div className="container-image">
-          <img src={img} />
+          <img src={img} className="img-podcasts" />
         </div>
         <div className="container-details">
           <p className="details-title">{title}</p>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.style.css";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const loader = useSelector((state) => state.podcasts.loader);
+
   return (
     <div className="container-header">
       <Link to="/" className="header-link">
-        <h1>Podcaster</h1>
+        <div className="head">
+          <h1>Podcaster</h1>
+          <div className="loader-container">
+            {loader && (
+              <div class="loader">
+                <div class="dot dot1"></div>
+                <div class="dot dot2"></div>
+                <div class="dot dot3"></div>
+              </div>
+            )}
+          </div>
+        </div>
       </Link>
     </div>
   );
