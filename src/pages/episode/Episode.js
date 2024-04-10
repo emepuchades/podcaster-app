5077b18cb72e243578f8e4ce537d342cf6cafe16
María Puchades <100128850+emepuchades@users.noreply.github.com>
2024-04-10 23:29:09 +0200

    fix: loader in episode

--- src/pages/episode/Episode.js
+++ src/pages/episode/Episode.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import CardPodcast from "../../components/CardPodcast/CardPodcast";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import PodcastPlayer from "../../components/PodcastPlayer/PodcastPlayer";
+import { fetchPodcastsLoader } from "../../redux/slice/podcastSlice";
 
 function Episode() {
   const { id, idepisode } = useParams();
+  const dispatch = useDispatch();
   const podcastsData = localStorage.getItem("podcasts");
   const podcasts = podcastsData ? JSON.parse(podcastsData) : [];
   const selectedPodcast = useSelector((state) => {
@@ -15,12 +17,17 @@ function Episode() {
   const podcastDetails = podcastsDetailsData
     ? JSON.parse(podcastsDetailsData)
     : [];
-  const selectedPodcastDetails =podcastDetails.find((podcast) => podcast.id === id)
-  const selectedEpisodie = 
-    selectedPodcastDetails.results.find(
-      (episodie) => episodie.trackId === parseInt(idepisode)
-    )
-  
+  const selectedPodcastDetails = podcastDetails.find(
+    (podcast) => podcast.id === id
+  );
+  const selectedEpisodie = selectedPodcastDetails.results.find(
+    (episodie) => episodie.trackId === parseInt(idepisode)
+  );
+
+  useEffect(() => {
+    dispatch(fetchPodcastsLoader(false));
+  }, [selectedEpisodie]);
+
   return (
     <div className="container-podcast-details">
       <CardPodcast
